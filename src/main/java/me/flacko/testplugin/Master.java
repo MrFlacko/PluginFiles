10f4a1984db613cdee5776f3855be3db031bc38b
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2020-09-04 23:22:15 +1000

    Add files via upload

---
+++ src/main/java/me/flacko/testplugin/Master.java
@@ -0,0 +1,20 @@
+package me.flacko.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This is a plugin!
+ */
+public class Master extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Put stuff here
+    }
+
+    @Override
+    public void onDisable() {
+        // Put stuff here
+    }
+
+}
